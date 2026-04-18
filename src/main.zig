79804e2db915b10b839e18c6d8dc9378edfdd11a
allexanderbergmans <allexander.bergmans@student.elisa.be>
2026-04-18 10:41:41 +0200

    A: Small initial code.

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,47 @@
 const std = @import("std");
+const Sha256 = std.crypto.hash.sha2.Sha256;
 
+const Block = struct {
+    index: u64,
+    timestamp: i64,
+    prev_hash: [32]u8,
+    nonce: u64,
+    hash: [32]u8,
+};
 
-const Blockchain = struct {
-    version: []const u8,                        // How else would we specifiy a string?
-    author: []const u8,                         // How else would we specifiy a sting?
+fn calculateHash(block: Block) [32]u8 {
+    var hasher = Sha256.init(.{});
 
+    hasher.update(std.mem.asBytes(&block.index));
+    hasher.update(std.mem.asBytes(&block.timestamp));
+    hasher.update(&block.prev_hash);
+    hasher.update(std.mem.asBytes(&block.nonce));
 
+    var out: [32]u8 = undefined;
+    hasher.final(&out);
+    return out;
+}
+
+fn createGenesisBlock() Block {
+    var block = Block{
+        .index = 0,
+        .timestamp = 0,
+        .prev_hash = [_]u8{0} ** 32, // no previous block
+        .nonce = 0,
+        .hash = undefined,
+    };
+
+    block.hash = calculateHash(block);
+    return block;
 }
 
 pub fn main() void {
-    // Polka L1 Blockchain
     std.debug.print("Polka L1 Blockchain\n", .{});
 
-    // Build the blockchain
-    
+    const genesis = createGenesisBlock();
+
+    std.debug.print("Genesis Block:\n", .{});
+    std.debug.print("Index: {d}\n", .{genesis.index});
+    std.debug.print("Timestamp: {d}\n", .{genesis.timestamp});
+    std.debug.print("Hash: {x}\n", .{genesis.hash});
 }
