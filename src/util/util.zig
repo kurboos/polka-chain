b30e7e3a3eeb1a0b6a347924a520a179272019b7
allexanderbergmans <allexander.bergmans@student.elisa.be>
2026-05-23 19:30:15 +0200

    * Added a util.zig.

---
+++ src/util/util.zig
@@ -0,0 +1,6 @@
+const std: type = @import("std");
+pub const Sha256 = std.crypto.hash.sha2.Sha256;
+
+fn utilityFunction() void {
+    // This is a placeholder for utility functions that can be used across the project.
+}
