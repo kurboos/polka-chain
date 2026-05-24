4d03884fcc332de89377e7bdaf682a2c46e0ec6f
allexanderbergmans <allexander.bergmans@student.elisa.be>
2026-05-24 11:10:45 +0200

    * Few fixes.

--- build.zig
+++ build.zig
@@ -4,25 +4,16 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "polka",
+    const root_module = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    // Declare modules (Zig’s way of creating clean boundaries)
-    // const node_mod = b.addModule("node", .{ .root_source_file = b.path("src/node/node.zig") });
-    // const network_mod = b.addModule("network", .{ .root_source_file = b.path("src/network/p2p.zig") });
-  
-
-    // Wire dependencies
-    // node_mod.addImport("network", network_mod);
- 
-
-    exe.root_module.addImport("node", node_mod);
+    const exe = b.addExecutable(.{
+        .name = "polka",
+        .root_module = root_module,
+    });
 
     b.installArtifact(exe);
-
-    // Add run, test, benchmark steps...
 }
