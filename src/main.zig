35c8aafcf697417f18d94cc76e7dfdd6252ca783
allexanderbergmans <allexander.bergmans@student.elisa.be>
2026-05-25 17:13:23 +0200

    chore: automated commit by polka

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,8 @@ const Sha256 = std.crypto.hash.sha2.Sha256; // Sha256 is the encryption library 
 
 // Block: struct, structure of a block.
 const Block = struct {
+    // Use this index to keep track of the block's
+    // position in the blockchain.
     index: u64, // The block index.
     timestamp: i64, // the timestamp
     prev_hash: [32]u8, // the previous hash
@@ -11,19 +13,26 @@ const Block = struct {
 };
 
 fn calculateHash(block: Block) [32]u8 {
-    var hasher = Sha256.init(.{});
+    var hasher = Sha256.init(.{}); // Initialize the SHA-256 hasher.
 
-    hasher.update(std.mem.asBytes(&block.index));
-    hasher.update(std.mem.asBytes(&block.timestamp));
-    hasher.update(&block.prev_hash);
-    hasher.update(std.mem.asBytes(&block.nonce));
+    hasher.update(std.mem.asBytes(&block.index)); // Update hasher with the block index.
+    hasher.update(std.mem.asBytes(&block.timestamp)); // Update hasher with the block timestamp.
+    hasher.update(&block.prev_hash); // Update hasher with the previous block's hash.
+    hasher.update(std.mem.asBytes(&block.nonce)); // Update hasher with the block nonce.
 
-    var out: [32]u8 = undefined;
-    hasher.final(&out);
-    return out;
+    // varible output is defined as an array of 32 bytes.
+    // The output buffer is then intialized as undefined, which means
+    // it stores uninitialized data.
+    var out: [32]u8 = undefined; // Output buffer
+    hasher.final(&out); // Finalize the hasher with the final output.
+    // The empty, undefined output buffer is passed to the final function,
+    // which fills it with the computed hash value.
+    return out; // Return the computed hash of the block.
 }
 
-fn createGenesisBlock() Block {
+fn createGenesisBlock() Block { // We need to pass in block because
+    // we need to calculate the hash of the block, which requires the block's fields
+    // to be defined.
     var block = Block{
         .index = 0,
         .timestamp = 0,
