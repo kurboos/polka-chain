53f3f4a09a887df412a005ca129fda9b0e4b39b0
allexanderbergmans <allexander.bergmans@student.elisa.be>
2026-05-25 10:52:47 +0200

    chore: automated commit by polka

--- src/types/utl.zig
+++ src/types/utl.zig
@@ -1,1 +1,13 @@
-/// SOON: This will contain utility functions and types that can be used across the project.
+const std = @import("std");
+
+const Hash = [32]u8;
+const Address = [20]u8;
+const PublicKey = [32]u8;
+
+/// Hash two 32-byte inputs together
+pub fn hashCombine(a: Hash, b: Hash) Hash {
+    var combined: [64]u8 = undefined;
+    @memcpy(combined[0..32], &a);
+    @memcpy(combined[32..64], &b);
+    return std.crypto.hash.Blake3.hash(&combined, .{});
+}
